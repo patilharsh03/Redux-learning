e0fed769986b97d70901652449fb0817ff569238
Harsh Patil <harshpatil223@gmail.com>
2023-03-20 16:41:17 +0530

    advanced-redux-and-rtk-query

---
+++ advanced-redux-and-rtk-query/src/main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { extendedApiSlice } from "./features/posts/postsSlice";
+import { fetchUsers } from "./features/users/usersSlice";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+store.dispatch(extendedApiSlice.endpoints.getPosts.initiate());
+store.dispatch(fetchUsers());
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </Provider>
+  </React.StrictMode>
+);
